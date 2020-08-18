9842d514129c547d5664346f325d9bd6489d0ee2
senluo <3175784674@qq.com>
2020-08-18 20:46:01 +0800

    注册和登录完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,53 @@ $(function() {
         $('.login-box').show();
         $('.reg-box').hide();
     })
+    var form = layui.form;
+    var layer = layui.layer;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码为6-12位，且不包含空格'],
+        repwd: function(value) {
+            var pass = $('.reg-box input[name=password]').val();
+            if (pass !== value) {
+                return "两次密码输入不一致"
+            }
+        }
+    })
+
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/api/reguser',
+            data: {
+                username: $('.reg-box [name=username]').val(),
+                password: $('.reg-box [name=password]').val(),
+            },
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg("注册成功，请登录");
+                $('#link_login').click();
+                $('#form_reg')[0].reset();
+
+            }
+        });
+    })
+
+    $('#form_login').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: $(this).serislize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg("登陆成功");
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html';
+            }
+        })
+    })
 })
